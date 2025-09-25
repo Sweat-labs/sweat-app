8dcec72414e04eae75d278cf6309ece779f3cefe
Anthony Jimenez <AnthonyJ1321@csu.fullerton.edu>
2025-09-24 22:00:00 -0700

    Mobile app all

---
+++ sweat-app-blank/app/contact.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+import { Colors } from "../constants/Color"
+import { useColorScheme } from 'react-native'
+
+const contact = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background } ]}> 
+      <Text style={styles.title}>Contact page</Text>
+      <Link href="/" style={styles.link}>Back to Home</Link>
+    </View>
+
+  )
+}
+
+export default contact
+
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center'
+},
+title: {
+  fontWeight: 'bold',
+  fontSize: 24
+
+},
+   link: {
+    marginVertical: 10,
+    borderBottimWidth: 1,
+    textDecorationLine: 'underline',
+    textDecorationColor: 'black'
+   },
+})
---
+++ sweat-app-blank/constants/Color.js
@@ -0,0 +1,25 @@
+export const Colors = {
+    primary: "#6849a7",
+    warning: "#cc475a",
+  
+    dark: {
+      text: "#d4d4d4",
+      title: "#fff",
+      background: "#252231",
+      navBackground: "#201e2b",
+      iconColor: "#9591a5",
+      iconColorFocused: "#fff",
+      uiBackground: "#2f2b3d",
+    },
+  
+    light: {
+      text: "#625f72",
+      title: "#201e2b",
+      background: "#6EC1E4",
+      navBackground: "#6EC1E4",
+      iconColor: "#684977",
+      iconColorFocused: "#201e2b",
+      uiBackground: "#d6d5e1",
+    },
+  }
+  
